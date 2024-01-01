d269955eae6bb2fcb0c0b3d8cd19098c5ee301d4
LiuHaowei <2239827165@qq.com>
2024-01-01 17:43:05 +0800

    element-ui,axios

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,38 @@
+<!--  -->
+<template>
+  <div class='layout-block'>
+    <el-button type="primary">hhh</el-button>
+    <el-button type="success">成功按钮</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  data() {
+    //这里存放数据
+    return {
+
+    }
+  },
+  computed: {},
+
+  watch: {},
+
+  methods: {
+
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {
+
+  },
+  updated() { }, //生命周期 - 更新之后
+}
+</script>
+
+<style lang='scss' scoped></style>
